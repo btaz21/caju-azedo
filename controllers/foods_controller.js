9760b8022dcf20a7fc1369420ccd457fab893797
Bobby Tazioli <bobby@Bobbys-MacBook-Pro.local>
2020-06-07 01:51:34 -0500

    careless cat has become a less careless cat

--- controllers/foods_controller.js
+++ controllers/foods_controller.js
@@ -29,16 +29,11 @@ router.get('/new', isAuthenticated, (req, res) => {
 
 // CREATE
 router.post('/', isAuthenticated, (req, res) => {
-  User.findByIdAndUpdate(req.session.currentUser._id, {addedFoods:req.body}, {new:true}, (error, updatedUser) => {
+  User.findByIdAndUpdate(req.session.currentUser._id, { $push: {addedFoods:req.body} }, {new:true}, (error, updatedUser) => {
     console.log(updatedUser);
     res.redirect('/foods')
   })
 })
-
-  // Food.create(req.body, (error, createdFood) => {
-  //   console.log(createdFood);
-  //   res.redirect('/foods')
-  // })
 
 
 
@@ -55,24 +50,11 @@ router.get('/', isAuthenticated, (req, res) => {
   })
 })
 
-  // Food.find({}, (error, allFoods) => {
-  //   res.render(
-  //     'food/index.ejs',
-  //     {
-  //       foods:allFoods,
-  //       currentUser: req.session.currentUser
-  //     }
-  //   )
-  // })
-
 
 
 // SHOW
 router.get('/:id/:altid', isAuthenticated, (req, res) => {
-  console.log(req.params.id);
-  console.log(req.params.altid);
   User.findById(req.params.id, (error, foundUser) => {
-    console.log(foundUser);
     res.render(
       'food/show.ejs',
       {
@@ -83,16 +65,6 @@ router.get('/:id/:altid', isAuthenticated, (req, res) => {
       }
     )
   })
-
-  // Food.findById(req.params.id, (error, foundFood) => {
-  //   res.render(
-  //     'food/show.ejs',
-  //     {
-  //       food:foundFood,
-  //       currentUser: req.session.currentUser
-  //     }
-  //   )
-  // })
 })
 
 
@@ -112,16 +84,6 @@ router.get('/:userId/:id/:altid/edit', isAuthenticated, (req, res) => {
 })
 
 
-  // Food.findById(req.params.id, (error, foundFood) => {
-  //   res.render(
-  //     'food/edit.ejs',
-  //     {
-  //       food:foundFood,
-  //       currentUser: req.session.currentUser
-  //     }
-  //   )
-  // })
-
 
 // UPDATE
 router.put('/:userId/:id/', (req, res) => {
@@ -133,27 +95,29 @@ router.put('/:userId/:id/', (req, res) => {
   })
 })
 
-//   Food.findByIdAndUpdate(req.params.id, req.body, (error, updatedFood) => {
-//     res.redirect('/foods')
-//   })
-// })
-
 
 // DELETE
-router.delete('/:id', isAuthenticated, (req, res) => {
-  Food.findByIdAndRemove(req.params.id, (error, deletedFood) => {
-    console.log(deletedFood)
+router.delete('/:userId/:id/:altId', isAuthenticated, (req, res) => {
+  User.findByIdAndUpdate(req.params.userId, { $pull: { addedFoods: { _id: req.params.id}}}, {new: true}, (error, foundUser) => {
+    console.log(error);
+    console.log(foundUser);
     res.redirect('/foods')
   })
 })
 
 
+
 // SEED
-router.get('/seed/starterdata', isAuthenticated, (req, res) => {
-  Food.create(seed, (error, data) => {
-    res.redirect('/')
-    }
-  )
+router.get('/seed/starterdata/fill/maybe/yes', isAuthenticated, (req, res) => {
+  User.findByIdAndUpdate(req.session.currentUser, { $push: {addedFoods:seed} }, {new:true}, (error, updatedUser) => {
+    console.log(updatedUser);
+      res.redirect('/')
+  })
+  // Food.create(seed, (error, data) => {
+  //   console.log(error);
+  //   }
+  // )
+
 })
 
 
--- models/seed.js
+++ models/seed.js
@@ -1,11 +1,24 @@
 const seed = [
   {
-    name: "Beijinho",
-    short_description: "",
-    description: "",
+    name: "Canjica",
+    description: "The dish is a porridge made with white de-germed whole maize kernels (canjica), cooked with milk, sugar and cinnamon until tender. Coconut and coconut milk as well as some cloves are also added, mainly in the north/northeastern variety of this recipe. Other ingredients may be added, such as peanuts and sweetened condensed milk.",
     etymology: "",
-    img: "https://i.imgur.com/HxdL3B3.png",
-    recipe: ""
+    img: "https://i.imgur.com/m4DYUfM.jpg",
+    recipe: "https://i.imgur.com/lCnz9pK.png"
+  },
+  {
+    name: "Moqueca",
+    description: "Moqueca is a Brazilian seafood stew slowly cooked in a terracotta cassole. It can be made with shrimp or fish as a base with tomatoes, onions, garlic, lime and coriander. The name moqueca comes from the term mu'keka in Kimbundu language.",
+    etymology: "",
+    img: "https://i.imgur.com/9zkbDW9.jpg",
+    recipe: "https://i.imgur.com/sce6iT1.png"
+  },
+  {
+    name: "Pão de Queijo",
+    description: "Pão de queijo is a small, baked cheese roll or cheese bun and a popular snack and breakfast food in Brazil. It is a traditional Brazilian recipe, from the state of Minas Gerais. Though its origins are uncertain, it is speculated that the recipe has existed since the eighteenth century, although it became popular around the 1950s.With the discovery of mines near Ouro Preto around 1700, 1/5 of the Brazilian population, mainly slaves, occupied a vast territory, moving from the Northeast and shifting the economic hub of the colony towards the Southeast.The mining cycle caused a huge impact and stimulus to the production of staple foods and this is when cheese bread was created. The Northeast and the nearest regions obtained beans, rice, corn and its cornmeal, pork and lard, milk and cheese. More distant areas such as the gaucho pampa, began to offer kind of beef meat. It is said that the cheese bread was offered by slave women to the farmers. At the time, wheat flour, the raw material of classical baking, was hard to find. Typical of temperate regions, the ancient cereal never combined with the warmth of the Brazilian Northeast, and was then imported from Europe to Brazil to serve the King's noblemen.Creatively, Minas Gerais cooks replaced the non-existent wheat with starch derived from cassava tuber with tupiniquins origins. Added the mass cured cheese chips, hardened and grated, and taken to the oven, turned out to like being called bread",
+    etymology: "",
+    img: "https://i.imgur.com/uhiSp8q.jpg",
+    recipe: "https://i.imgur.com/mIMpTxh.png"
   }
 ]
 
